d81e2c868a290902bc7b12615afb97869062d457
Rosalie Bruel <Rosalie.Bruel@uvm.edu>
2020-01-04 14:14:40 -0500

    edited the 3 functions to allow input of a dataframe

--- R/sampling_iterative.R
+++ R/sampling_iterative.R
@@ -5,42 +5,70 @@
 #' @export
 #' @param x Initial vector
 #' @param n Number of samples analyzed
+#' @param n2 Initial number of sample, regularly spaced. Default to 5. Will be changed to (n2*2-1) if no changepoints were detected for initial value of n2.
+#' @param messages Print changepoint or not
+#' @param input_vector Default input vector = T. If turned to F, allow a data frame
+#' @param xcol if input_vector = F, index of columns not to use for ordination.
+#' @param DCA_axis which DCA axis to use as vector on which to detect changepoint
 #' @keywords sampling
 #' @examples 
 #'
 
-sample_iterative <- function(x, n, messages = T)
-  .sample_iterative(x, n, messages)
-.sample_iterative <- function(x, n, messages) {
+sample_iterative <- function(x, n, n2 = 5, messages = T, input_vector = T, xcol = NA, DCA_axis = 1)
+  .sample_iterative(x, n, messages, input_vector, xcol, DCA_axis)
+.sample_iterative <- function(x, n, messages, input_vector, xcol, DCA_axis) {
   require(ecp)
   require(zoo)
+  require(vegan)
+  do_dca <- function(dataset, xcol= NA) {
+    data_temp <- dataset[,-c(xcol)]
+    data_temp <- data_temp[, colSums(data_temp != 0) > 0]
+    data_temp <- decorana(data_temp)
+    return(as.data.frame(data_temp$rproj))
+  }
   
-  if(!is.vector(x)) stop("x is required and must be a vector")
-  if(is.null(n)) n <- length(x)
+  x_stable <- x
+  
+  if(!is.vector(x)&input_vector) stop("x is required and must be a vector, unless input_vector is turned to FALSE. If so, x must be a dataframe.")
+  if(input_vector) lastpoint = length(x) else lastpoint = nrow(x)
+  if(is.null(n)) n <- lastpoint
   
   # Sample initial number of samples
-  n2 = 5
-  which_samples <- seq(1,length(x),length.out = n2)
+  which_samples <- seq(1,lastpoint,length.out = n2)
   which_samples <- round(which_samples)
   which_samples <- which_samples[order(which_samples)]
   
+  # Do DCA to get only one vector if input is a data frame
+  if(!input_vector) {
+    x <- do_dca(x[which_samples,], xcol)
+    x <- x[,DCA_axis]
+  }
   
   # transform into matrix to run e.divisive
-  x2 <- matrix(c(1:length(x),rep(NA,length(x))), ncol=2)
-  x2[which_samples,2] <- x[which_samples]
+  x2 <- matrix(c(1:lastpoint,rep(NA,lastpoint)), ncol=2)
+  if(input_vector) x2[which_samples,2] <- x[which_samples] else x2[which_samples,2] <- x
   x2[,2] <- na.locf(x2[,2])
   x2 <- matrix(x2[,2])
   
   temp_chgpt <- e.divisive(x2, min.size = 2, sig.lvl = .7)$order.found[3]
   
   if(is.na(temp_chgpt)) {
-    which_samples <- seq(1,2,length(x),length.out = n2*2-1)
+    which_samples <- seq(1,2,lastpoint,length.out = n2*2-1)
     which_samples <- round(which_samples)
     which_samples <- which_samples[order(which_samples)]
     
+    # Reset x
+    x <- x_stable
+    
+    # Do DCA to get only one vector if input is a data frame
+    if(!input_vector) {
+      x <- do_dca(x[which_samples,], xcol)
+      x <- x[,DCA_axis]
+    }
+    
     # transform into matrix to run e.divisive
-    x2 <- matrix(c(1:length(x),rep(NA,length(x))), ncol=2)
-    x2[which_samples,2] <- x[which_samples]
+    x2 <- matrix(c(1:lastpoint,rep(NA,lastpoint)), ncol=2)
+    if(input_vector) x2[which_samples,2] <- x[which_samples] else x2[which_samples,2] <- x
     x2[,2] <- na.locf(x2[,2])
     x2 <- matrix(x2[,2])
     
@@ -54,9 +82,18 @@ sample_iterative <- function(x, n, messages = T)
       which_samples <- c(which_samples, round((which_samples[which(which_samples==temp_chgpt)-1]+which_samples[which(which_samples==temp_chgpt)])/2))
       which_samples <- which_samples[order(which_samples)]
       
+      # Reset x
+      x <- x_stable
+      
+      # Do DCA to get only one vector if input is a data frame
+      if(!input_vector) {
+        x <- do_dca(x[which_samples,], xcol)
+        x <- x[,DCA_axis]
+      }
+      
       # transform into matrix to run e.divisive
-      x2 <- matrix(c(1:length(x),rep(NA,length(x))), ncol=2)
-      x2[which_samples,2] <- x[which_samples]
+      x2 <- matrix(c(1:lastpoint,rep(NA,lastpoint)), ncol=2)
+      if(input_vector) x2[which_samples,2] <- x[which_samples] else x2[which_samples,2] <- x
       x2[,2] <- na.locf(x2[,2])
       x2 <- matrix(x2[,2])
       
@@ -70,14 +107,16 @@ sample_iterative <- function(x, n, messages = T)
   if(messages) print(changepoints$order.found[3])
   
   # More elaborated output
-                  out_list <- list()
-      out_list$changepoint <- changepoints$order.found[3]
-          out_list$final_n <- length(which_samples)
-  out_list$changepoint_all <- changepoints
-           out_list$matrix <- data.frame("index"=which_samples,"x"=x[which_samples])
+                           out_list <- list()
+               out_list$changepoint <- changepoints$order.found[3]
+                   out_list$final_n <- length(which_samples)
+           out_list$changepoint_all <- changepoints
+   if(input_vector) out_list$matrix <- data.frame("index"=which_samples,"x"=x[which_samples])
+  if(!input_vector) out_list$matrix <- data.frame("index"=which_samples,"x"=x)
   
   invisible(out_list)
 } 
 
 test <- sample_iterative(ar1, n = 10)
 e.divisive(matrix(test$matrix$x), min.size = 2)$order.found[3]
+
--- R/sampling_random.R
+++ R/sampling_random.R
@@ -5,26 +5,44 @@
 #' @export
 #' @param x Initial vector
 #' @param n Number of samples analyzed
+#' @param messages Print changepoint or not
+#' @param input_vector Default input vector = T. If turned to F, allow a data frame
+#' @param xcol if input_vector = F, index of columns not to use for ordination.
+#' @param DCA_axis which DCA axis to use as vector on which to detect changepoint
 #' @keywords sampling
 #' @examples 
 #'
 
-sample_random <- function(x, n, messages = T)
-  .sample_random(x, n, messages)
-.sample_random <- function(x, n, messages) {
+sample_random <- function(x, n, messages = T, input_vector = T, xcol = NA, DCA_axis = 1)
+  .sample_random(x, n, messages, input_vector, xcol, DCA_axis)
+.sample_random <- function(x, n, messages, input_vector, xcol, DCA_axis) {
   require(ecp)
   require(zoo)
+  require(vegan)
+  do_dca <- function(dataset, xcol= NA) {
+    data_temp <- dataset[,-c(xcol)]
+    data_temp <- data_temp[, colSums(data_temp != 0) > 0]
+    data_temp <- decorana(data_temp)
+    return(as.data.frame(data_temp$rproj))
+  }
   
-  if(!is.vector(x)) stop("x is required and must be a vector")
-  if(is.null(n)) n <- length(x)
+  if(!is.vector(x)&input_vector) stop("x is required and must be a vector, unless input_vector is turned to FALSE. If so, x must be a dataframe.")
+  if(input_vector) lastpoint = length(x) else lastpoint = nrow(x)
+  if(is.null(n)) n <- lastpoint
   
   # Sample n samples from initial x
-  which_samples <- c(1,length(x),sample(1:length(x), n-2))
+  which_samples <- c(1,lastpoint,sample(1:lastpoint, n-2))
   which_samples <- which_samples[order(which_samples)]
   
+  # Do DCA to get only one vector if input is a data frame
+  if(!input_vector) {
+    x <- do_dca(x[which_samples,], xcol)
+    x <- x[,DCA_axis]
+  }
+  
   # transform into matrix to run e.divisive
-  x2 <- matrix(c(1:length(x),rep(NA,length(x))), ncol=2)
-  x2[which_samples,2] <- x[which_samples]
+  x2 <- matrix(c(1:lastpoint,rep(NA,lastpoint)), ncol=2)
+  if(input_vector) x2[which_samples,2] <- x[which_samples] else x2[which_samples,2] <- x
   x2[,2] <- na.locf(x2[,2])
   x2 <- matrix(x2[,2])
   
@@ -34,11 +52,11 @@ sample_random <- function(x, n, messages = T)
   if(messages) print(changepoints$order.found[3])
   
   # More elaborated output
-                  out_list <- list()
-      out_list$changepoint <- changepoints$order.found[3]
-          out_list$final_n <- length(which_samples)
-  out_list$changepoint_all <- changepoints
-           out_list$matrix <- data.frame("index"=which_samples,"x"=x[which_samples])
-  
+                           out_list <- list()
+               out_list$changepoint <- changepoints$order.found[3]
+                   out_list$final_n <- length(which_samples)
+           out_list$changepoint_all <- changepoints
+   if(input_vector) out_list$matrix <- data.frame("index"=which_samples,"x"=x[which_samples])
+  if(!input_vector) out_list$matrix <- data.frame("index"=which_samples,"x"=x)  
   invisible(out_list)
 } 
