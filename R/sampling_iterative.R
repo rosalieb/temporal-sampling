cd33f0129c3d1a8180d0e6ea5765c87a9c49206b
Rosalie Bruel <Rosalie.Bruel@uvm.edu>
2020-10-02 14:27:36 -0400

    add the possibility in the function to edit R (permutation number) and sig.lvl, for the function e.divisive, wrapped into each of the sampling function

--- R/sampling_iterative.R
+++ R/sampling_iterative.R
@@ -11,13 +11,16 @@
 #' @param input_vector Default input vector = T. If turned to F, allow a data frame
 #' @param xcol if input_vector = F, index of columns not to use for ordination.
 #' @param DCA_axis which DCA axis to use as vector on which to detect changepoint
+#' @param R number of permutation in the e.divisive function from the package ecp
+#' @param sig.lvl significance level in the e.divisive function from the package ecp
 #' @keywords sampling
 #' @examples 
 #'
 
-sample_iterative <- function(x, n, n2 = 5, c=1, messages = T, input_vector = T, xcol = NA, DCA_axis = 1)
-  .sample_iterative(x, n, n2, c, messages, input_vector, xcol, DCA_axis)
-.sample_iterative <- function(x, n, n2, c, messages, input_vector, xcol, DCA_axis) {
+sample_iterative <- function(x, n, n2 = 5, c=1, messages = T, input_vector = T, xcol = NA, DCA_axis = 1,
+                             R = 199, sig.lvl = 0.05)
+  .sample_iterative(x, n, n2, c, messages, input_vector, xcol, DCA_axis, R, sig.lvl)
+.sample_iterative <- function(x, n, n2, c, messages, input_vector, xcol, DCA_axis, R, sig.lvl) {
   require(ecp)
   require(zoo)
   require(vegan)
@@ -51,7 +54,7 @@ sample_iterative <- function(x, n, n2 = 5, c=1, messages = T, input_vector = T, 
   if(any(is.na(x2[,2]))) x2[,2] <- na.locf(x2[,2])
   x2 <- matrix(x2[,2])
   
-  temp_chgpt <- e.divisive(x2, min.size = 2, sig.lvl = .7)$order.found[3:(2+c)]
+  temp_chgpt <- e.divisive(x2, min.size = 2, R = R, sig.lvl = .7)$order.found[3:(2+c)]
   
   if(all(is.na(temp_chgpt)) && length(which_samples) < n ) {
     which_samples <- seq(1,2,lastpoint,length.out = n2*2-1)
@@ -73,7 +76,7 @@ sample_iterative <- function(x, n, n2 = 5, c=1, messages = T, input_vector = T, 
     if(any(is.na(x2[,2]))) x2[,2] <- na.locf(x2[,2])
     x2 <- matrix(x2[,2])
     
-    temp_chgpt <- e.divisive(x2, min.size = 2, sig.lvl = .7)$order.found[3:(2+c)]
+    temp_chgpt <- e.divisive(x2, min.size = 2, R = R, sig.lvl = .7)$order.found[3:(2+c)]
   }
   
   # Now, start adding samples
@@ -101,7 +104,7 @@ sample_iterative <- function(x, n, n2 = 5, c=1, messages = T, input_vector = T, 
           if(any(is.na(x2[,2]))) x2[,2] <- na.locf(x2[,2])
           x2 <- matrix(x2[,2])
           
-          temp_chgpt <- e.divisive(x2, min.size = 2, sig.lvl = .7)$order.found[3:(2+c)]
+          temp_chgpt <- e.divisive(x2, min.size = 2, R = R, sig.lvl = .7)$order.found[3:(2+c)]
         } else {
           c_order = c_order + 1
         }
@@ -109,11 +112,19 @@ sample_iterative <- function(x, n, n2 = 5, c=1, messages = T, input_vector = T, 
     }
   }
   
-  changepoints <- e.divisive(x2, min.size = 2)
+  changepoints <- e.divisive(x2, min.size = 2, R = R, sig.lvl = sig.lvl)
   if(length(changepoints$p.values)>1) changepoints$p.values <- changepoints$p.values[-length(changepoints$p.values)]
+  if(length(changepoints$p.values)>1) changepoints$estimates <- changepoints$estimates[-c(1, length(changepoints$estimates))]
   
   # Print the main changepoint
   if(messages) print(changepoints$order.found[3])
+  
+  mmessage <- paste0("We used the function e.divisive() in the package ecp to calculate the changepoint(s). ",
+                     "The original function let the user choose R (the number of permutation, default = 199) and sig.lev (the significance level, default = 0.05). ",
+                     "Herein, the two parameters are: R = ", R, ", and sig.lvl = ", sig.lvl,". ",
+                     "p-values in the output are calculated during the permutation process by the e.divisive function. ",
+                     "p = 0.005 is the lowest p possible when R = 199, as it means that for each of the 199 permutations,  the energy needed to break the matrix was lower than the energy needed to find the changepoint in the initial configuration. ",
+                     "See the e.divisive docmentation for more details.")
   
   # More elaborated output
                            out_list <- list()
@@ -121,6 +132,7 @@ sample_iterative <- function(x, n, n2 = 5, c=1, messages = T, input_vector = T, 
                out_list$changepoint <- changepoints$order.found[3]
                    out_list$final_n <- length(which_samples)
            out_list$changepoint_all <- changepoints
+                   out_list$message <- mmessage
    if(input_vector) out_list$matrix <- data.frame("index"=which_samples,"x"=x[which_samples])
   if(!input_vector) out_list$matrix <- data.frame("index"=which_samples,"x"=x)
   
--- R/sampling_regular.R
+++ R/sampling_regular.R
@@ -9,13 +9,16 @@
 #' @param input_vector Default input vector = T. If turned to F, allow a data frame
 #' @param xcol if input_vector = F, index of columns not to use for ordination.
 #' @param DCA_axis which DCA axis to use as vector on which to detect changepoint
+#' @param R number of permutation in the e.divisive function from the package ecp
+#' @param sig.lvl significance level in the e.divisive function from the package ecp
 #' @keywords sampling
 #' @examples 
 #'
 
-sample_regular <- function(x, n, messages = T, input_vector = T, xcol = NA, DCA_axis = 1)
-  .sample_regular(x, n, messages, input_vector, xcol, DCA_axis)
-.sample_regular <- function(x, n, messages, input_vector, xcol, DCA_axis) {
+sample_regular <- function(x, n, messages = T, input_vector = T, xcol = NA, DCA_axis = 1,
+                           R = 199, sig.lvl = 0.05)
+  .sample_regular(x, n, messages, input_vector, xcol, DCA_axis, R, sig.lvl)
+.sample_regular <- function(x, n, messages, input_vector, xcol, DCA_axis, R, sig.lvl) {
   require(ecp)
   require(zoo)
   require(vegan)
@@ -48,11 +51,19 @@ sample_regular <- function(x, n, messages = T, input_vector = T, xcol = NA, DCA_
   if(any(is.na(x2[,2]))) x2[,2] <- na.locf(x2[,2])
   x2 <- matrix(x2[,2])
   
-  changepoints <- e.divisive(x2, min.size = 2)
+  changepoints <- e.divisive(x2, min.size = 2, R = R, sig.lvl = sig.lvl)
   if(length(changepoints$p.values)>1) changepoints$p.values <- changepoints$p.values[-length(changepoints$p.values)]
+  if(length(changepoints$p.values)>1) changepoints$estimates <- changepoints$estimates[-c(1, length(changepoints$estimates))]
   
   # Print the main changepoint
   if(messages) print(changepoints$order.found[3])
+  
+  mmessage <- paste0("We used the function e.divisive() in the package ecp to calculate the changepoint(s). ",
+                     "The original function let the user choose R (the number of permutation, default = 199) and sig.lev (the significance level, default = 0.05). ",
+                     "Herein, the two parameters are: R = ", R, ", and sig.lvl = ", sig.lvl,". ",
+                     "p-values in the output are calculated during the permutation process by the e.divisive function. ",
+                     "p = 0.005 is the lowest p possible when R = 199, as it means that for each of the 199 permutations,  the energy needed to break the matrix was lower than the energy needed to find the changepoint in the initial configuration. ",
+                     "See the e.divisive docmentation for more details.")
   
   # More elaborated output
                            out_list <- list()
@@ -60,6 +71,7 @@ sample_regular <- function(x, n, messages = T, input_vector = T, xcol = NA, DCA_
                out_list$changepoint <- changepoints$order.found[3]
                    out_list$final_n <- length(which_samples)
            out_list$changepoint_all <- changepoints
+                   out_list$message <- mmessage
    if(input_vector) out_list$matrix <- data.frame("index"=which_samples,"x"=x[which_samples])
   if(!input_vector) out_list$matrix <- data.frame("index"=which_samples,"x"=x)  
   
